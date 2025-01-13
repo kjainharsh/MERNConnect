7457d53654c2d75b1b8d75e15799042863832f79
kjainharsh <kjainharsh@gmail.com>
2025-01-13 23:35:40 +0530

    nothing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ export const Home = () => {
                                 <a href="/contact">
                                     <button className="btn">connect now</button>
                                 </a>
-                                <a href="/services">
+                                <a href="/service">
                                     <button className="btn secondary-btn">learn more</button>
                                 </a>
                             </div>
@@ -67,7 +67,7 @@ export const Home = () => {
                             <a href="/contact">
                                 <button className="btn">connect now</button>
                             </a>
-                            <a href="/services">
+                            <a href="/service">
                                 <button className="btn secondary-btn">learn more</button>
                             </a>
                         </div>
