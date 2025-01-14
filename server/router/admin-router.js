242133b130404601f1b086e06e41755db3867435
kjainharsh <kjainharsh@gmail.com>
2025-01-14 21:23:31 +0530

    Adding Admin Contacts Route

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const getAllUsers = require("../controllers/admin-controller");
+const getAllContacts = require("../controllers/admin_contact-controller");
 const router = express.Router();
 
 router.route('/users').get(getAllUsers);
+router.route('/contacts').get(getAllContacts);
 
 module.exports = router;
