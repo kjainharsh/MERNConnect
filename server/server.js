72c17a2cd708c0a115d4a45f9b876375e9f910b7
kjainharsh <kjainharsh@gmail.com>
2025-01-14 21:18:10 +0530

    Admin Dashboard

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoute = require("./router/auth-router.js");
 const contactRoute = require("./router/contact-router.js");
 const serviceRoute = require("./router/service-router.js");
+const adminRoute = require("./router/admin-router.js");
 const connectdb = require("./utils/db.js");
 const errorMiddleware = require("./middlewares/error-middlewares.js");
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
 app.use("/api/data", serviceRoute);
+app.use("/api/admin", adminRoute);
 app.use(errorMiddleware);
 
 const PORT = 5000;
